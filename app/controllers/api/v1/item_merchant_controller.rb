53bf6f78e2433881f1f0925169808455bd871893
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-02 00:19:29 -0600

    add item merchant controller for route to pass

---
+++ app/controllers/api/v1/item_merchant_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ItemMerchantController < ApplicationController
+  before_action :item
+
+  def show
+    merchant = item.merchant
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+  def item
+    Item.find(params[:item_id])
+  end
+end
