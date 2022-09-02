8897e849652f1c8181eb503afed5375d69fa828b
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-02 00:23:51 -0600

    add item factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Device.model_name }
+    description { Faker::Books::Lovecraft.sentence }
+    unit_price { Faker::Number.decimal }
+    merchant { create(:merchant) }
+  end
+end
