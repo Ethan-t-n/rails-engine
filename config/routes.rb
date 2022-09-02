de284eb356d7f067ba5aadafec1d286931938bd6
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-02 00:31:00 -0600

    add and update/refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'items/find_all', to: 'items/search#index'
+      get 'merchants/find', to: 'merchants/search#show'
+
+        # get '/merchants/find', to: 'search#merchant'
+        # get '/merchants/find_all', to: 'search#all_merchants'
+        # get '/items/find_all', to: 'search#all_items'
+        # get '/items/find', to: 'search#item'
+
+      resources :merchants, only: [:index, :show] do
+        get '/items', to: 'merchant_items#index'
+      end
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        get '/merchant', to: 'item_merchant#show'
+      end
+    end
+  end
 end
+
+
